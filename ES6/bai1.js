b505a9263ff6b71ec868e6710026d0c9ce01cd36
thonguyenp <nphuoctho2406@gmail.com>
2025-09-03 21:34:19 +0700

    Học tới phần 10p35 chap 1

--- ES6/bai1.js
+++ ES6/bai1.js
@@ -1,1 +1,17 @@
-alert('me');
+// alert('me');
+
+var name = "Hoi Dan IT";
+
+var age = 25; //kieu du lieu so, tam vuc: global
+
+console.log("Check info: ", name, age);
+
+function test ()
+{
+    var x = 10;
+    console.log("Check info: ", name, age); 
+}
+
+console.log("Check info: ", x); 
+
+// test();
