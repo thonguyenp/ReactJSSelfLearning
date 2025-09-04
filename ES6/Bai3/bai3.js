619b5dff26357eb7bd085de875279982042ec78c
thonguyenp <nphuoctho2406@gmail.com>
2025-09-04 18:10:27 +0700

    Hoàn thành bài về mảng

--- ES6/Bai3/bai3.js
+++ ES6/Bai3/bai3.js
@@ -1,3 +1,24 @@
 let person = {name: "Tho", age: 21};    //kieu du lieu mang
 
-console.log("Check var: ", person);
+let person2 = {
+    name: "Tho", 
+    age: 21,
+    eyeColor: "black",
+    "My address": "984 duong 23/10"
+};    //xuống hàng cho tường minh
+
+// console.log("Check var: ", person2);
+
+const nguoi = ["Tho", "Nguyen", 21];
+
+// console.log("Check first name: ", person2.name);
+// console.log("Check first name: ", person2["name"]); //truy cap 1 thuoc tinh chua biet truoc
+console.log("Check name", nguoi[0]);
+
+//exercise 
+let React = {
+    language: "javascript",
+    author: "facebook"
+};
+
+alert(React.language);
