9c187a729f791c62474e8c24d79449c0a28c40e7
thonguyenp <nphuoctho2406@gmail.com>
2025-09-04 20:28:06 +0700

    Toán tử mũi tên để rút gọn hàm

---
+++ ES6/Bai4/bai4.js
@@ -0,0 +1,15 @@
+// function test ()
+// {
+//     console.log("Test");
+// }
+
+//Lam code ngan hon
+test = () => console.log("Test");
+
+hello = (value, value2) =>  "Hello world!" + value;
+
+// test();
+console.log(hello("Tho"));
+
+//exercise
+const hello2 = () => "Hello";
