f58d684d7a73743773af11670abc0d24e8a2643b
thonguyenp <nphuoctho2406@gmail.com>
2025-09-05 11:59:49 +0700

    Hoàn thành chủ đề về template string

--- ES6/Bai6/bai6.js
+++ ES6/Bai6/bai6.js
@@ -1,39 +1,60 @@
-// Bên trong map() và filter () đều là 1 func 
+const s1 = "Hello world";
 
-const myArr = [1,2,3,4];
+const s2 = 'Hello world';
 
-// const myList = myArr.map((item) => item * 2);  //jsx 
-//map () tao 1 arr mới từ arr ban đầu
+const s3 = `Hello world`;
 
-//Bên trong hàm map() là 1 func
-//const  a = () => {};
-const myList = myArr.map((item, index) => 
-{
-    // console.log(item, index);
-    return item * 2;
-});
+// console.log(s1,s2,s3);   //3 chuỗi nằm trên cùng 1 dòng
 
-//Viết theo kiểu tạo thêm 1 arr mới
-const newArr = []
-for (let i = 0; i < myArr.length; i++)
-{
-    let item = myArr[i] * 2;
-    newArr.push(item);
-}
+//dấu back-ticks giúp điền như nào thì render trên body y hệt
+// console.log(`str1
+//     str2`); 
 
-// console.log(myList,newArr);
+let a = 5;
+let b = 10;
 
-//Filter làm giảm số lượng phần tử ban đầu theo 1 tiêu chí
-const ages = [32, 33, 16, 40];
+console.log(`Fifteen is ${a+b} and 
+not ${2 * a + b}`); //dùng ${} khi để tính toán biến khi trong template string
 
-// const result = ages.filter(chkAdult);
-// function chkAdult (age)
-// {
-//     return age >= 18;
-// }
+//exercise
+const base_url = "localhost:8080";
+const api = "get-user"; fetch_page = 2;
 
-const result = ages.filter((age) => {
-    return age >= 18;
-});
+const result = `${base_url}/${api}?page=${fetch_page}`;
 
 console.log(result);
+
+
+
+
+const premierLeague = [
+  { club: 'MC', score: 100 },
+  { club: 'Liverpool', score: 98 },
+  { club: 'Chelsea', score: 88 },
+  { club: 'MU', score: 69 }
+];
+
+let count = premierLeague.length;
+
+// Make some strings to include in our output.
+let tableHeader = 
+`<table>
+    <tr>
+        <th>Club</th>
+        <th>Score</th>
+    </tr>
+`;
+let tableContent = "";
+
+// Loop through the JSON and output each row in to a string.
+for (i = 0; i < count; i++) {
+  tableContent += 
+    `<tr>
+        <td> ${premierLeague[i].club}</td>
+        <td> ${premierLeague[i].score}</td>
+    </tr>`;
+}
+
+let tableFooter = "</table>";
+// Get div and output the HTML. You can include these HTML strings straight in to your emailText variable.
+document.getElementById("tableTest").innerHTML = tableHeader + tableContent + tableFooter;
